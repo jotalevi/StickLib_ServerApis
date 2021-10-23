c3b1e63f5a1c0564ace8258126519338fb0af771
jotalevi <talevineto@gmail.com>
2021-10-23 20:44:04 -0300

    fixing stuff

--- classes/user/code/class.php
+++ classes/user/code/class.php
@@ -90,10 +90,7 @@ function userNew_route(){
 function userLogin_route(){
     $jsonData = json_decode(file_get_contents('php://input'), true);
 
-    return array(
-        'username'=>$jsonData['username'],
-        'password'=>$jsonData['password']
-    );
+    return json_encode($jsonData);
     
     $usr = User::fromHash(hash('sha256', $jsonData['username'] . $jsonData['password']));
     if ($usr->userId != 0){
